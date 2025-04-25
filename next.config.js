96033663f383d1189a705e642a334fed5d9e8626
mail2robbins <mail2robbins@gmail.com>
2025-04-24 21:06:13 -0400

    disables generating static pages in nextjs build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
   },
+  // Configure page extensions to exclude API routes from static generation
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  // Ensure API routes are not statically generated
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
